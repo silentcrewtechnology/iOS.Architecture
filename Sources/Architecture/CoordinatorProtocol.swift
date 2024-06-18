42393a7d964b318398492610181824a55b92e0f6
Firdavs Turdiev <TurdievFKh@akbarsdigital.ru>
2024-06-18 05:45:31 +0300

    escaping

--- Sources/Architecture/CoordinatorProtocol.swift
+++ Sources/Architecture/CoordinatorProtocol.swift
@@ -3,6 +3,6 @@ import Foundation
 
 public protocol CoordinatorProtocol {
     
-    func setupFlow(completion: @escaping Closure<Any>)
+    func setupFlow(completion: @escaping Closure<Any?>)
     func startFlow()
 }
